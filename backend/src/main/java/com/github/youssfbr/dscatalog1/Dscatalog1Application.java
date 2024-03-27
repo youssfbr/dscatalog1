5b2054fb0dfa557832b1449b925b0f85e936cf18
Alisson Youssf <youssfbr@gmail.com>
2024-03-27 12:09:44 -0300

    Project backend created

---
+++ backend/src/main/java/com/github/youssfbr/dscatalog1/Dscatalog1Application.java
@@ -0,0 +1,13 @@
+package com.github.youssfbr.dscatalog1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dscatalog1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dscatalog1Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/github/youssfbr/dscatalog1/Dscatalog1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.youssfbr.dscatalog1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dscatalog1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
