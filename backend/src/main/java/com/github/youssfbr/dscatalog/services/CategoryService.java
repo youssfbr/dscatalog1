ca132b14b9a247d347f2b337ed125d3595a620e4
Alisson Youssf <youssfbr@gmail.com>
2024-03-27 19:59:35 -0300

    Database seeding

--- backend/src/main/java/com/github/youssfbr/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/github/youssfbr/dscatalog/services/CategoryService.java
@@ -3,6 +3,7 @@ package com.github.youssfbr.dscatalog.services;
 import com.github.youssfbr.dscatalog.entities.Category;
 import com.github.youssfbr.dscatalog.repositories.CategoryRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,6 +15,7 @@ public class CategoryService {
         this.categoryRepository = categoryRepository;
     }
 
+    @Transactional(readOnly = true)
     public List<Category> findAll() {
         return categoryRepository.findAll();
     }
